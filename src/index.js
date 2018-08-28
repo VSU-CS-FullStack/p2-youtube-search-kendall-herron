57d7b2f993ad1931adbf4fd62692c6ce2446452b
kendallherron <ktherron@valdosta.edu>
2018-08-28 12:12:48 -0400

    class component

--- src/index.js
+++ src/index.js
@@ -6,5 +6,13 @@ import registerServiceWorker from "./registerServiceWorker";
 
 const API_KEY = "AIzaSyBNWxTGfnnsvn7CacFilp4vVRv6SOKAbEE";
 
+const App = () => {
+	return (
+		<div>
+			<SearchBar />
+		</div>
+	);
+};
+
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
