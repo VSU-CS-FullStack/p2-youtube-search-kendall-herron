a70a6df0bf3233fd2042342c34561c7f8647704b
kendallherron <ktherron@valdosta.edu>
2018-08-30 12:17:51 -0400

    ajax requests with react

--- src/index.js
+++ src/index.js
@@ -1,19 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
+// import React from "react";
 import ReactDOM from "react-dom";
 import SearchBar from "./components/search_bar";
-// import "./index.css";
-// import App from "./App";
-// import registerServiceWorker from "./registerServiceWorker";
+import YTSearch from "youtube-api-search";
 
 const API_KEY = "AIzaSyCq0zxXoVnlu9k5QtwKQvK0kIZifhwKeL8";
 
-const App = () => {
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	);
-};
-// App();
+// const App = () => {
+// 	return (
+// 		<div>
+// 			<SearchBar />
+// 		</div>
+// 	);
+// };
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { videos: [] };
+		YTSearch({ key: API_KEY, term: "surfboards" }, videos => {
+			// ES6 syntax for this.setState({ videos: videos});
+			this.setState({ videos });
+		});
+	}
+	render() {
+		return (
+			<div>
+				<SearchBar />
+			</div>
+		);
+	}
+}
+
 ReactDOM.render(<App />, document.getElementById("root"));
-// registerServiceWorker();
